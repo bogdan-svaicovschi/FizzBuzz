935352f7d7fe90f3e8bc8440c41e4e2e57e37d99
Bogdan Svaicovschi <bogdan.svaicovschi@softwire.com>
2026-07-08 12:27:30 +0300

    removed unwanted code

--- index.ts
+++ index.ts
@@ -1,29 +1,7 @@
 
 // This is our main function
 function fizzbuzz(): void {
-    // for(let i = 0; i < 100; i++) {
-    //     let output : String = "";
-    //    if (i % 3 == 0) {
-    //     output += "Fizz";
-    //    }
-    //    if (i % 5 == 0) {
-    //     output += "Buzz";
-    //    }
-    //    if (i % 7 == 0) {
-    //     output += "Bang";
-    //    }
-    //    if (i % 11 == 0) {
-    //      output = "Bong";
-    //    }
-    //    if (i % 13 == 0) {
-        
-    //    }
-    //    if (output == "") {
-    //     console.log(i);
-    //    } else {
-    //     console.log(output);
-    //    }
-    // }
+
     for (let i = 1; i <= 200; i ++) {
         let output: String[] = [];
         if (i % 3 == 0) {
